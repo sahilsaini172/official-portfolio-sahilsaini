d1c2c43cc5ca027f60d09d87bbe70793c7da2d95
Sahil Saini <sahilmanisaini8398@gmail.com>
2025-12-20 19:12:03 +0530

    gsap navitem

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,33 +1,48 @@
 "use client";
 import Link from "next/link";
 import DarkModeToggle from "./ThemeToggle";
+import gsap from "gsap";
+import { useEffect } from "react";
 
 export default function Navbar() {
+  useEffect(() => {
+    var tl = gsap.timeline();
+    tl.from("#navitem", {
+      y: -500,
+      opacity: 0,
+      duration: 1.5,
+      stagger: 0.3,
+    });
+  });
   return (
     <header className="sticky bg-transparent p-8 flex items-center justify-between">
       <div></div>
       <nav className="flex items-center gap-4 2xl:text-2xl 2xl:gap-16 *:2xl:font-semibold xl:text-xl xl:gap-7 *:xl:font-medium lg:text-lg lg:gap-6">
         <Link
           href={"/work"}
-          className="hover:text-primary hover:scale-110 ease-in duration-100"
+          id="navitem"
+          className="hover:text-primary hover:scale-110 w-fit"
         >
           Work
         </Link>
         <Link
+          id="navitem"
           href={"/about"}
-          className="hover:text-primary hover:scale-110 ease-in duration-100"
+          className="hover:text-primary hover:scale-110 w-fit"
         >
           About
         </Link>
         <Link
+          id="navitem"
           href={"/blog"}
-          className="hover:text-primary hover:scale-110 ease-in duration-100"
+          className="hover:text-primary hover:scale-110 "
         >
           Blog
         </Link>
         <Link
+          id="navitem"
           href={"/contact"}
-          className="hover:text-primary hover:scale-110 ease-in duration-100"
+          className="hover:text-primary hover:scale-110 "
         >
           Contact
         </Link>
